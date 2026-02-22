a768d9f03a2db9a9d16f7ee5cea32aed54ca2405
sid370578 <sid370578@gmail.com>
2026-02-22 15:26:19 +0530

    Removed duplicate crud-with-cassandra from concurrency folder

--- concurrency-and-threading/crud-with-cassandra/src/main/java/com/example/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todo.repository;
-
-import com.example.todo.model.Todo;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface TodoRepository extends CassandraRepository<Todo, String> {
-}
